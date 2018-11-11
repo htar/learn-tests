c068197ced9b32318570c5e02b8b092dcc14026f
Taras <khmelovski@gmail.com>
2018-11-11 21:10:44 +0200

    added 'describe'

--- server/server.test.js
+++ server/server.test.js
@@ -2,26 +2,32 @@ const request = require('supertest');
 const app = require('./server').app;
 var expect = require('expect');
 
-it('should return name response', (done) => {
-    request(app).get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
-})
 
-it('should return users obj', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Taras',
-                age: 29
-            });
-        })
-        .end(done);
-})
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return name response', (done) => {
+            request(app).get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+    describe('GET /users', () => {
+        it('should return users obj', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Taras',
+                        age: 29
+                    });
+                })
+                .end(done);
+        });
+    });
+});
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,59 +1,63 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two number', () => {
-    const res = utils.add(33, 11);
-    // throw new Error('Value not correct');
-    // if (res !== 44) {
-    //     throw new Error(`Expected 44, but get ${res}`);
-    // }
-    expect(res).toBe(44).toBeA('number'); 
-});
 
-
-it('Should square a number', () => {
-    let num = 2;
-    const res = utils.square(num);
-    // if (res !== num * num) {
-    //     throw new Error(`Expected ${num * num}, but get ${res}`);
-
-    // }
-    expect(res).toBe(4).toBeA('number');
-});
-
-it('should expect some values',() => {
-    expect(12).toNotBe(22);
-    expect({name:'Taras'}).toEqual({name:'Taras'});
-    expect({name:'taras'}).toNotEqual({name:'Taras'});
-    expect([1,2,3,4]).toInclude(4);
-    expect([1,2,3,4]).toExclude(5);
-    expect({name:'Taras', age: 29,})
-        .toInclude({age:29});
-});
-
-// should verify first   and last names areset
-// assert it includes firstName and lastName withproper values
-
-it('should set firstNAme and lastName',() =>{
-    const user = {location:'Lviv',age:29};
-    const res = utils.setName(user, 'Taras Khmelovskyi');
-    expect(user).toEqual(res);
-    expect(res).toInclude({
-        firstName: 'Taras'
+describe('Utils',() => {
+    it('should add two number', () => {
+        const res = utils.add(33, 11);
+        // throw new Error('Value not correct');
+        // if (res !== 44) {
+        //     throw new Error(`Expected 44, but get ${res}`);
+        // }
+        expect(res).toBe(44).toBeA('number'); 
     });
-});
-
-
-it('should async add two  number', (done) => {
-    utils.asyncAdd(4,3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    
+    
+    it('Should square a number', () => {
+        let num = 2;
+        const res = utils.square(num);
+        // if (res !== num * num) {
+        //     throw new Error(`Expected ${num * num}, but get ${res}`);
+    
+        // }
+        expect(res).toBe(4).toBeA('number');
     });
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(4, (square) => {
-        expect(square).toBe(16).toBeA('number');
-        done();
+    
+    it('should expect some values',() => {
+        expect(12).toNotBe(22);
+        expect({name:'Taras'}).toEqual({name:'Taras'});
+        expect({name:'taras'}).toNotEqual({name:'Taras'});
+        expect([1,2,3,4]).toInclude(4);
+        expect([1,2,3,4]).toExclude(5);
+        expect({name:'Taras', age: 29,})
+            .toInclude({age:29});
     });
-})
+    
+    // should verify first   and last names areset
+    // assert it includes firstName and lastName withproper values
+    
+    it('should set firstNAme and lastName',() =>{
+        const user = {location:'Lviv',age:29};
+        const res = utils.setName(user, 'Taras Khmelovskyi');
+        expect(user).toEqual(res);
+        expect(res).toInclude({
+            firstName: 'Taras'
+        });
+    });
+    
+    
+    it('should async add two  number', (done) => {
+        utils.asyncAdd(4,3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
+    });
+    
+    it('should async square a number', (done) => {
+        utils.asyncSquare(4, (square) => {
+            expect(square).toBe(16).toBeA('number');
+            done();
+        });
+    });
+
+});
